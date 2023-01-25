2974b94b65b0809198b26fe7309d247d13d6319f
Arthur Picht <picht@arthurpicht.de>
2023-01-25 16:53:39 +0100

    m7r-infra-user and m7r-instance in config dir

--- src/main/java/org/mentalizr/commons/paths/host/hostDir/M7rInstanceConfigFile.java
+++ src/main/java/org/mentalizr/commons/paths/host/hostDir/M7rInstanceConfigFile.java
@@ -2,15 +2,15 @@ package org.mentalizr.commons.paths.host.hostDir;
 
 import org.mentalizr.commons.paths.M7rFile;
 
-public class M7rInfraUserConfigFile extends M7rFile {
+public class M7rInstanceConfigFile extends M7rFile {
 
-    public static final String NAME = "m7r-infra-user.conf";
+    public static final String NAME = "m7r-instance.conf";
 
-    public static M7rInfraUserConfigFile createInstance() {
-        return new M7rInfraUserConfigFile();
+    public static M7rInstanceConfigFile createInstance() {
+        return new M7rInstanceConfigFile();
     }
 
-    private M7rInfraUserConfigFile() {
+    private M7rInstanceConfigFile() {
         super(M7rHostConfigDir.createInstance().asPath().resolve(NAME));
     }
 
