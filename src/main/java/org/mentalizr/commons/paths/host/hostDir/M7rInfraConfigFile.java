2d9de30e7bcc70d594cd1febfd49a5d2cbaee1a6
Arthur Picht <picht@arthurpicht.de>
2025-02-27 16:30:21 +0100

    rename SchedulerConfigFile to M7rInfraConfigFile

--- src/main/java/org/mentalizr/commons/paths/host/hostDir/M7rInfraConfigFile.java
+++ src/main/java/org/mentalizr/commons/paths/host/hostDir/M7rInfraConfigFile.java
@@ -3,10 +3,10 @@ package org.mentalizr.commons.paths.host.hostDir;
 import org.mentalizr.commons.paths.FileNames;
 import org.mentalizr.commons.paths.M7rFile;
 
-public class SchedulerConfigFile extends M7rFile {
+public class M7rInfraConfigFile extends M7rFile {
 
-    public SchedulerConfigFile() {
-        super(new M7rHostConfigDir(), FileNames.SCHEDULER_CONF);
+    public M7rInfraConfigFile() {
+        super(new M7rHostConfigDir(), FileNames.M7R_INFRA_CONF);
     }
 
 }
